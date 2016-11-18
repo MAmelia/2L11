baf91d22a38b4783f5f7306d33f6087777c5767e
Bruno <traderbbc@gmail.com>
2016-11-18 03:34:22 -0200

    Add content code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@ $(function(){
       interval: 3000
     });
 });
+
+ $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 100) {
+          $(this).addClass("slide");
+        }
+    });
+  });
